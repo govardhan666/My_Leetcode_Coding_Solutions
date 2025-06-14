b435b3e2eda292e8b4fb65904edb9dfaaaef70bd
Vaddi Govardhan <71170706+govardhan666@users.noreply.github.com>
2025-06-14 11:26:38 +0530

    Time: 1 ms (89.2%), Space: 40.7 MB (56.25%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -3,7 +3,7 @@ class Solution {
         String s = String.valueOf(num);
         char[] arr = s.toCharArray();
         char u = '9';
-        char l = '9';
+        char l = '0';
         for(char ch:arr){
             if(ch!='9') {
                 u=ch;
